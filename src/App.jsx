59223c0d627e1488f1e31ac4891280c3c0f206ec
miguelchaparrocolo <122495031+miguelchaparrocolo@users.noreply.github.com>
2023-04-25 19:24:53 -0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div class='404'><p>404 NOT FOUND</p></div>
+        <div class="image"><img src="https://i.postimg.cc/LXdmbnvv/espantapajaros-404.png"></img></div>
+          <div class="content">
+            <h1 class="line 1"> I have bad news for you </h1>
+            <p class="line 2"> 
+                The page you looking 
+                for might be removed or is
+                temporally unavaible 
+            </p> 
+            <a href="#"> BACK TO HOMEPAGE</a>
+          </div>    
+    </>
+  )
+}
+
+export default App
